0d6e37bcb5ef38cd9a1dbd08b35171a8bff09d5a
blackxsepia <159821702+blackxsepia@users.noreply.github.com>
2024-03-27 14:19:30 +0100

    cookiebanner nur einmal anzeigen

--- src/js/cookies.js
+++ src/js/cookies.js
@@ -1,8 +1,8 @@
  function acceptCookies() {
             // Hier könntest du Cookies setzen oder andere Aktionen durchführen
-            // Zum Beispiel: localStorage.setItem('cookiesAccepted', true);
+            localStorage.setItem('cookiesAccepted', true);
             document.getElementById('cookie-banner').style.display = 'none';
         }
 
-        // Hier könntest du prüfen, ob der Benutzer bereits Cookies akzeptiert hat und das Banner entsprechend ausblenden
-        // Zum Beispiel: if (localStorage.getItem('cookiesAccepted')) { document.getElementById('cookie-banner').style.display = 'none'; }
+        
+     if (localStorage.getItem('cookiesAccepted')) { document.getElementById('cookie-banner').style.display = 'none'; }
